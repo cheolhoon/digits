745ec58909865d013ac810744af66283a47bf252
Cheolhoon Choi <54978134+cheolhoon@users.noreply.github.com>
2020-11-05 14:35:33 -1000

    contacts collection finished in 49 minutes.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Grid, Icon, Header } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <div className="digits-landing-background">
-        <Grid container centered stacktable columns={3}>
+        <div className="digits-background">
+          <Grid container centered stackable columns={3}>
 
-          <Grid.Column textAlign='center'>
-            <Icon size="huge" name="users" inverted />
-            <Header as ='h1' inverted>Multiple Users</Header>
-            <Header as ='h3' inverted>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</Header>
-          </Grid.Column>
-          <Grid.Column textAlign='center'>
-            <Icon size="huge" name="file alternate" inverted />
-            <Header as ='h1' inverted>Contact Details</Header>
-            <Header as ='h3' inverted>For each contact, you can save their name, address, and phone number.</Header>
-          </Grid.Column>
-          <Grid.Column textAlign='center'>
-            <Icon size="huge" name="calender check" inverted />
-            <Header as ='h1' inverted>Timestamped Notes</Header>
-            <Header as ='h3' inverted>Each time you make contact with a contact, you can write a note that summarizes the conversation.
-              This note is saved along with a timestamp with the contact.</Header>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon name='users' size='huge' inverted/>
+              <Header as='h1' inverted>Multiple Users</Header>
+              <Header as='h3' inverted>This address book enables any number of users to register and save their business contacts. You can only see the contacts that you have created.</Header>
+            </Grid.Column>
 
-        </Grid>
+            <Grid.Column textAlign='center'>
+              <Icon name='file alternate' size='huge' inverted/>
+              <Header as='h1' inverted>Contact details</Header>
+              <Header as='h3' inverted>For each contact, you can save their name, address, and phone number.</Header>
+            </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+              <Icon name='calendar check outline' size='huge' inverted/>
+              <Header as='h1' inverted>Timestamped Notes</Header>
+              <Header as='h3' inverted>Each time you make a contact with a contact, you can write a note that
+                summarizes that conversation. This note is saved along with a timestamp with the contact.</Header>
+            </Grid.Column>
+
+          </Grid>
         </div>
     );
   }
